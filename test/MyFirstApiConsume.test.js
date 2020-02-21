f23e09f3d2e7cc2cf1433c3fd8dd9f556654e590
SebastianUrbano <john.urbano@correo.icesi.edu.co>
2020-02-21 08:56:46 -0500

    test with new recommend changes

--- test/MyFirstApiConsume.test.js
+++ test/MyFirstApiConsume.test.js
@@ -41,27 +41,28 @@ describe('First Api Tests', () => {
    expect(response.body).to.have.property('headers');
   });
   it('Consume PATCH Service with body', async () => {
-    const response = await agent.patch('https://httpbin.org/patch').query(query)
+    const response = await agent.patch('https://httpbin.org/patch').send(query)
    
     expect(response.status).to.equal(statusCode.OK);
     expect(response.body).to.have.property('headers');
-    expect(response.body.args).to.eql(query);
+    expect(JSON.parse(response.body.data)).to.eql(query);
    });
   
   it('Consume PUT Service send person', async () => {
-    const response = await agent.put('https://httpbin.org/put').query(query);
+    const response = await agent.put('https://httpbin.org/put').send(query);
   
     expect(response.status).to.equal(statusCode.OK);
     expect(response.body).to.have.property('origin');
-    expect(response.body.args).to.eql(query);
+    expect(JSON.parse(response.body.data)).to.eql(query);
   });
  
   
   it('Consume DELETE Service', async () => {
-    const response = await agent.del('https://httpbin.org/delete');
+    const response = await agent.del('https://httpbin.org/delete').send(query);
   
     expect(response.status).to.equal(statusCode.OK);
     expect(response.body).to.have.property('json');
+
   });
   
 });
